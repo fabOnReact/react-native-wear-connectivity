a94d5b26ce821bce3402277784cf90f6dc45a708
Fabrizio Bertoglio <fabrizio.bertoglio@gmail.com>
2024-02-08 16:45:48 +0800

    testing multiply

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,15 +1,37 @@
 import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import LoginScreen from './LoginScreen';
 import WearScreen from './WearScreen';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import { multiply } from 'react-native-wear-connectivity';
 
 const App = () => {
+  const [result, setResult] = React.useState<number | undefined>();
+  React.useEffect(() => {
+    multiply(3, 7).then(setResult);
+  }, []);
   return (
     <>
       <LoginScreen />
       <WearScreen />
+      <View style={styles.container}>
+        <Text>Result: {result}</Text>
+      </View>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  box: {
+    width: 60,
+    height: 60,
+    marginVertical: 20,
+  },
+});
 
 export default App;
