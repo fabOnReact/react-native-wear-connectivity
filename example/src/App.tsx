076829caa557d549c820afa45fb781b9dbdd438f
Fabrizio Bertoglio <fabrizio.bertoglio@gmail.com>
2024-02-08 15:09:29 +0800

    updating example and remove lefthook

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 import { multiply } from 'react-native-wear-connectivity';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
+  const [count, setCount] = React.useState(0);
 
   React.useEffect(() => {
     multiply(3, 7).then(setResult);
@@ -12,7 +13,11 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Text>count is: {count}</Text>
+      <TouchableOpacity
+        onPress={() => setCount((currentCount) => currentCount + 1)}
+        style={styles.button}
+      />
     </View>
   );
 }
@@ -22,10 +27,18 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'yellow',
   },
   box: {
     width: 60,
     height: 60,
     marginVertical: 20,
+  },
+  button: {
+    marginTop: 50,
+    height: 50,
+    width: 50,
+    backgroundColor: 'red',
+    borderRadius: 50,
   },
 });
