169e42ab537de21b724f0517b4967e199ef4aea4
Fabrizio Bertoglio <fabrizio.bertoglio@gmail.com>
2024-08-04 13:33:44 +0800

    update contributing 2 take (#28)

--- example/src/CounterScreen/index.android.tsx
+++ example/src/CounterScreen/index.android.tsx
@@ -49,7 +49,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     padding: 8,
-    backgroundColor: 'yellow',
+    backgroundColor: 'red',
   },
   counter: {
     height: 100,
