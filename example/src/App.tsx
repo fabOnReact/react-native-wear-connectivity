167fd88a5281493dc405a97979cc34405b278864
Fabrizio Bertoglio <fabrizio.bertoglio@gmail.com>
2025-07-29 16:45:17 +0200

    updating android 14 permissions (#50)

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
-import { Platform } from 'react-native';
+import { Platform, PermissionsAndroid } from 'react-native';
 import { default as CounterScreenAndroid } from './CounterScreen/index.android';
 import { default as CounterScreenIos } from './CounterScreen/index.ios';
 
+async function requestBluetoothPermissions() {
+  if (Platform.OS === 'android' && Platform.Version >= 31) {
+    const granted = await PermissionsAndroid.requestMultiple([
+      PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+      PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+    ]);
+
+    console.log('Bluetooth permissions:', granted);
+  }
+}
+
 const App = () => {
+  React.useEffect(() => {
+    requestBluetoothPermissions();
+  }, []);
+
   const CounterScreen =
     Platform.OS === 'ios' ? CounterScreenIos : CounterScreenAndroid;
   return <CounterScreen />;
