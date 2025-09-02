9143e4438ea816e076df3b3f07a4c8ff34b56d62
Fabrizio Bertoglio <fabrizio.bertoglio@gmail.com>
2025-09-02 16:35:52 +0200

    test: add basic placeholder

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-react',
+    '@babel/preset-typescript',
+  ],
 };
---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,5 @@
+describe('basic test', () => {
+  it('passes', () => {
+    expect(1 + 1).toBe(2);
+  });
+});
